26e51dc82152d3464d1f94124c8244fef71cc164
Abhay Aditya Dubey <63297421+aad99bxp@users.noreply.github.com>
2023-07-22 20:13:40 +0530

    Add files via upload

---
+++ TrainSchedule/src/App.js
@@ -0,0 +1,14 @@
+// src/App.js
+import React from 'react';
+import './App.css';
+import TrainSchedule from './Components/TrainSchedule';
+
+function App() {
+  return (
+      <div className="App">
+        <TrainSchedule />
+      </div>
+  );
+}
+
+export default App;
